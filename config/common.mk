3776651ae23df7605d91121c552549a06e0a9c20
Pranav Vashi <neobuddy89@gmail.com>
2016-12-22 01:25:12 +0530

    vendor: Add and enable OMS dependency
    Change-Id: Iae5965426db378101154bff6c21971a4f272425b
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -253,7 +253,11 @@ PRODUCT_COPY_FILES += \
 
 # Custom crDroid packages
 PRODUCT_PACKAGES += \
-    OmniSwitch
+    OmniSwitch \
+    masquerade
+
+PRODUCT_PROPERTY_OVERRIDES := \
+    ro.substratum.verified=true
 
 #DU Utils Library
 PRODUCT_PACKAGES += \
